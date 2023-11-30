fdc14794ef1711d50b9c958d0b860cbda5eeaca1
Given <moretigiven@gmail.com>
2023-11-30 15:34:22 +0200

    Fix Card Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
-
-import './styles.css';
+import "./styles.css";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -9,22 +8,39 @@ import Footer from "./components/Footer";
 import Card from "./components/Card";
 
 function App() {
-
   const [name, setName] = useState("Given");
 
   const date = new Date();
   const hours = date.getHours();
+  let timeOfDay;
+  if (hours > 12) {
+    timeOfDay = "Good Afternoon";
+  } else if (hours > 18) {
+    timeOfDay = "Good Evening";
+  } else if (hours > 20) {
+    timeOfDay = "Good Night";
+  } else {
+    timeOfDay = "Good Morning";
+  }
 
   return (
     <div className="App">
       <Header />
       <Main />
-        <h1> {(hours > 12) ? "Good Afternoon" : "Good morning"} {name}</h1>
-      <Card />
-      <Card />
-      
-      <Footer />
+      <h1>
+        {" "}
+        {timeOfDay} {name}
+      </h1>
+      <Card img="./images/twitch.png"
+        title="my first disk"
+        description="gta 5"
+      />
+      <Card img="./images/ocean-bottom-logo.png"
+        title="my second disk"
+        description="gta 6"
+      />
 
+      <Footer />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
 
 
 
     return (
         <div>
-            <div className='card'>
-                <img src="../images/twitch.png" alt="" />
-
-                <h1 className='card-title'>title</h1>
-                <p className='card-description'>description</p>
-                <input className="pokeBtn" type='button' value="poke seller" />
-
+            <div  className='card-content'>
+                <div className='card'>
+                    <img src={props.img} alt="" />
+                    <h1 className='card-title'>{props.title}</h1>
+                    <p className='card-description'>{props.description}</p>
+                    <input className="pokeBtn" type='button' value="poke seller" />
+                </div>
             </div>
 
         </div>
